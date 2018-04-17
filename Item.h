e97c12944a9a0e2af628ea3ef4dfeb6f9578fa0b
Arcoy <30359770+Arcoy@users.noreply.github.com>
2018-04-16 18:19:29 -0700

    Began adding member vars

--- Item.h
+++ Item.h
@@ -1,8 +1,15 @@
-#pragma once
+#include <string>
+using namespace std;
+
 class Item
 {
+private:
+   string name;
+   int weight, uses, maxUses;
+   bool equiped;
 public:
    Item();
-   ~Item();
+   Item(string name, int weight, int maxUses);
+   bool use();
 };
 
