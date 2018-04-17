5cc577fffe860ce26be7d1e1d79f9a4ef6c150be
Arcoy <30359770+Arcoy@users.noreply.github.com>
2018-04-17 15:54:03 -0700

    Added setToDefault

--- Item.h
+++ Item.h
@@ -4,12 +4,16 @@ using namespace std;
 class Item
 {
 private:
+   int id;
    string name;
    int weight, uses, maxUses;
    bool equiped;
+   int statBonus[7]; //If an equiped item grants bonus stats, they are stored here
 public:
    Item();
-   Item(string name, int weight, int maxUses);
-   bool use();
+   Item(int id); //read from the Item.txt to find id, then load Item
+   Item(string name, int weight, int maxUses); // make a new temp Item
+   bool use(int statMods[7]); //parameter is the using unit's bonus stat array
+   void setToDefault();
 };
 
