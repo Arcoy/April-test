1d6f249ce017190f8af0f8a7236b6c3e4aa907ac
Arcoy <30359770+Arcoy@users.noreply.github.com>
2018-04-16 18:20:31 -0700

    Various edits
    Implemented stat mod array, fixed and cleaned toString

--- Unit.cpp
+++ Unit.cpp
@@ -7,16 +7,42 @@
 string Unit::toString()
 {
    stringstream strm;
-   strm << "Name: " << name << endl;
-   strm << "Class: " << unitClass << endl;
-   strm << "str " << str << endl;
-   strm << "dex " << dex << endl;
-   strm << "con " << con << endl;
-   strm << "wis " << wis << endl;
-   strm << "int " << intel << endl;
-   strm << "cha " << cha << endl;
-   strm << "skill " << skill << endl;
+   strm << name << endl;
+   strm << unitClass << " level " << level << endl;
    strm << "Hp: " << hp << "/" << maxHp << endl;
+   strm << "str " << stats[0];
+   if (statmods[0] != 0)
+      strm << " +" << statmods[0];
+   strm << endl;
+   strm << "dex " << stats[1];
+   if (statmods[2] != 0)
+      strm << " +" << statmods[2];
+   strm << endl;
+   strm << "con " << stats[2];
+   if (statmods[2] != 0)
+      strm << " +" << statmods[2];
+   strm << endl;
+
+   strm << "wis " << stats[3];
+   if (statmods[3] != 0)
+      strm << " +" << statmods[3];
+   strm << endl;
+
+   strm << "int " << stats[4];
+   if (statmods[4] != 0)
+      strm << " +" << statmods[4];
+   strm << endl;
+
+   strm << "cha " << stats[5];
+   if (statmods[5] != 0)
+      strm << " +" << statmods[5];
+   strm << endl;
+
+   strm << "skl " << stats[6];
+   if (statmods[6] != 0)
+      strm << " +" << statmods[6];
+   strm << endl;
+
    return strm.str();
 }
 Unit::Unit()
@@ -28,29 +54,33 @@ Unit::Unit(string name, string unitClass, int level, int str, int dex, int con, 
    this->name = name;
    this->unitClass = unitClass;
    this->level = level;
-   this->str = str;
-   this->dex = dex;
-   this->con = con;
-   this->wis = wis;
-   this->intel = intel;
-   this->cha = cha;
-   this->skill = skill;
+   stats[0] = str;
+   stats[1] = dex;
+   stats[2] = con;
+   stats[3] = wis;
+   stats[4] = intel;
+   stats[5] = cha;
+   stats[6] = skill;
+   for (int i = 0; i < 7; i++)
+   {
+      statmods[i] = 0;
+   }
    update();
 }
 bool Unit::attack(Unit &target)
 {
    int chance = (hitRate - target.avoid) + 50;
    int randNum = (rand() % 100 + 1 + rand() % 100 + 1) / 2;
-   if (randNum >= chance)
+   if (randNum <= chance)
       target.damage(1);
    return true;
 }
 
 bool Unit::update()
 {
-   hitRate = skill + (2 * level);
-   avoid = dex + (2 * level);
-   maxHp = con + (2 * level);
+   hitRate = stats[6] + (2 * level) + statmods[6];
+   avoid = stats[1] + (2 * level) + statmods[1];
+   maxHp = stats[2] + (2 * level) + statmods[2];
    hp = maxHp;
    return true;
 }
